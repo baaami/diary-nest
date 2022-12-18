18031a81f52145c8b25ea1022ff9ea919db5bb2d
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-12-18 17:49:27 +0900

    post module화

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostController } from './post/post.controller';
 import { UserController } from './user/user.controller';
 import { AuthController } from './auth/auth.controller';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
-  controllers: [PostController, UserController, AuthController],
+  imports: [PostModule],
+  controllers: [UserController, AuthController],
   providers: [],
 })
 export class AppModule {}
