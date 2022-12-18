030a10339d41ee850248bd52c3d054ae85d5a88e
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-12-18 17:49:14 +0900

    auth controller 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor() {}
+  @Get('/callback/kakao')
+  read(): string {
+    return 'kakao hello';
+  }
+}
