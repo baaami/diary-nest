796874720961616553bfc41ff98d47f7ea8fd7ee
baaami <52270698+SeongBeom27@users.noreply.github.com>
2023-01-15 15:25:29 +0900

    애플리케이션에 ValidationPipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
@@ -6,6 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+			// decorator가 없는 어떤 property에 대해서 값이 input되지 못하게함
+      // whitelist: true,
+			// 존재하지 않는 property를 보낼 경우 체크
+      forbidNonWhitelisted: true,
+			// user들이 보낸 데이터를 실제 원하는 타입으로 변환해줌
+      transform: true,
+    }),
+  );
   await app.listen(4000);
 }
 bootstrap();
