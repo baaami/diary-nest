21c6ff3d4448711be8f5b9a5e1a51b2dd460c3c1
baaami <52270698+SeongBeom27@users.noreply.github.com>
2023-02-05 22:39:09 +0900

    content 테스트 코드 작성

--- src/api/content/content.service.spec.ts
+++ src/api/content/content.service.spec.ts
@@ -1,39 +1,101 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { Test, TestingModule } from '@nestjs/testing';
 import { Users } from '../user/entities/user.entity';
 import { Contents } from '../content/entities/content.entity';
 import { Images } from 'src/common/entities/image.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ContentService } from "./content.service";
-import * as ormconfig from '../../../ormconfig'
+import { ContentService } from './content.service';
+import { UserService } from 'src/api/user/user.service';
+import * as ormconfig from '../../../ormconfig';
+import { CreateContentDto } from './dto/create-content.dto';
+import { UpdateResult } from 'typeorm';
+import { UpdateContentDto } from './dto/update-content.dto';
 
 describe('ContentService', () => {
-    let service: ContentService;
-    beforeEach(async () => {
-      const module: TestingModule = await Test.createTestingModule({
-        imports: [
-          TypeOrmModule.forFeature([Users, Contents, Images]),
-          TypeOrmModule.forRoot(ormconfig),
-        ],
-        providers: [ContentService],
-      }).compile();
-      service = module.get<ContentService>(ContentService);
-    });
-  
-    it('should be defined', () => {
-      expect(service).toBeDefined();
-    });
+  let service: ContentService;
+  let userService: UserService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forFeature([Users, Contents, Images]),
+        TypeOrmModule.forRoot(ormconfig),
+      ],
+      providers: [ContentService, UserService],
+    }).compile();
+    service = module.get<ContentService>(ContentService);
+    userService = module.get<UserService>(UserService);
+  });
 
-    describe('get Content', () => {
-        it('should return a content', async () => {
-            const result = await service.findOne(6)
-            expect(result).toBeInstanceOf(Contents)
-        })
-    })
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 
-    describe('get Content List', () => {
-        it('should return a contents', async () => {
-            const result = await service.findList()
-            expect(result).toBeInstanceOf(Array)
-        })
-    })
+  describe('Create Content', () => {
+    it('create a content', async () => {
+      // given : 테스트를 하기 위한 환경 구성
+
+      // 1. image 데이터 생성
+      // 2. 글 생성
+      const user: Users = await userService.findlatest();
+      const files: { images?: Express.Multer.File[] } = {
+        images: null,
+      };
+
+      const createContentDto: CreateContentDto = {
+        title: 'jest title',
+        content: 'jest content',
+        userId: user.id,
+      };
+
+      // when : 테스트 함수 실행
+      const content: Contents = await service.Create(
+        createContentDto,
+        files,
+        user,
+      );
+
+      // then : 테스트 함수 결과
+      expect(content.title).toEqual(createContentDto.title);
+      expect(content.content).toEqual(createContentDto.content);
+    });
+  });
+
+  describe('Update Content', () => {
+    it('update a content', async () => {
+      // given : 테스트를 하기 위한 환경 구성
+
+      // 1. image 데이터 생성
+      // 2. 글 생성
+      const user: Users = await userService.findlatest();
+      const files: { images?: Express.Multer.File[] } = {
+        images: null,
+      };
+
+      const updateContentDto: UpdateContentDto = {
+        title: 'jest update title',
+        content: 'jest update content',
+        userId: user.id,
+      };
+
+      // when : 테스트 함수 실행
+      const updateContent: UpdateResult = await service.Update(
+        updateContentDto,
+        user.id,
+        files,
+      );
+    });
+  });
+
+  describe('get Content', () => {
+    it('should return a content', async () => {
+      const result = await service.findOne(6);
+      expect(result).toBeInstanceOf(Contents);
+    });
+  });
+
+  describe('get Content List', () => {
+    it('should return a contents', async () => {
+      const result = await service.findList();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
 });
