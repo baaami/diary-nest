342a1a6f7975c4612bdb95a0e14d26e53b2fb77e
baaami <lgh121546@naver.com>
2023-04-03 19:54:19 +0900

    관심 목록 페이지네이션 해결

--- src/api/favorite/favorite.controller.ts
+++ src/api/favorite/favorite.controller.ts
@@ -7,7 +7,7 @@ export class FavoriteController {
   
   // 특정 유저 관심 목록 리스트
   @Get(":id")
-  getFavoriteList(@Param("id", ParseIntPipe) userId: number, @Query() page: number = 0) {
+  getFavoriteList(@Param("id", ParseIntPipe) userId: number, @Query('page') page: number = 0) {
     if(isNaN(page)) page = 0
     return this.favoriteService.getFavoriteList(userId, page)
   }
