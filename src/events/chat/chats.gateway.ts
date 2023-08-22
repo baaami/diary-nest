1758231a2f0cc9dca2094a521290643b60fdb9ad
bookhabit <qnrgoqlt@naver.com>
2023-08-22 18:54:09 +0900

    join_room 후에 roomId 전송

--- src/events/chat/chats.gateway.ts
+++ src/events/chat/chats.gateway.ts
@@ -161,6 +161,7 @@ export class ChatGateway
     const bSocketInRoom = socket.rooms.has(roomId);
     if (bSocketInRoom == false) {
       socket.join(roomId);
+      socket.emit("roomId_after_join_room",roomId)
     }
     return;
   }
