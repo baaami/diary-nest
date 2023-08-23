17f944fcb33b6f0d983878955f2744340c856454
bookhabit <qnrgoqlt@naver.com>
2023-08-23 17:40:47 +0900

    채팅-이미지파일업로드 api 구현

--- src/api/photos/photos.controller.ts
+++ src/api/photos/photos.controller.ts
@@ -9,16 +9,15 @@ export class PhotosController {
   @UseInterceptors(FilesInterceptor('photos'))
   async uploadPhotos(@UploadedFiles() files: Express.Multer.File[]) {
     const uploadFiles: string[] = [];
-    console.log('전달받은 files',files)
+    
     for (const file of files) {
       const { buffer,originalname } = file;
       const parts = originalname.split('.');
       const ext = parts[parts.length - 1];
       const newName = 'upload/image_' + Date.now() + '.' + ext;
-      const uploadPath = path.join(__dirname, '..','..','..','..', newName); // 경로 수정
+      const uploadPath = path.join(__dirname, '..','..','..','..', newName);
+
       // 파일 데이터를 지정된 경로에 저장
-      console.log('_dirname',path.dirname)
-      console.log('uploadPath',uploadPath)
       await fs.writeFile(uploadPath, buffer,(err) => {
         if (err) throw err;
         console.log('The file has been saved!');
