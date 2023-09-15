ea4a3855baaa1e9c5f04eb77eb420050cfaec56f
baaami <lgh121546@naver.com>
2023-09-15 20:35:15 +0900

    알림 삭제, 확인 API 추가

--- src/api/notification/notification.controller.ts
+++ src/api/notification/notification.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from "@nestjs/common";
 import { NotificationService } from "./notification.service";
 import { NotificationList } from "src/common/entities/common.entity";
 
@@ -11,15 +19,39 @@ export class NotificationController {
     @Query("page") page: number = 0,
     @Param("id", ParseIntPipe) userId: number
   ) {
-    const [notification_list, totalPage] =
-      await this.notificationService.getNotificationListByUID(page, userId);
+    try {
+      const [notification_list, totalPage] =
+        await this.notificationService.getNotificationListByUID(page, userId);
 
-    const result: NotificationList = {
-      notification_list,
-      currentPage: page,
-      totalPage,
-    };
+      const result: NotificationList = {
+        notification_list,
+        currentPage: page,
+        totalPage,
+      };
 
-    return result;
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @HttpCode(204)
+  @Post("/confirm/:id")
+  async confirm(@Param("id", ParseIntPipe) notification_id: number) {
+    try {
+      await this.notificationService.confirmNotification(notification_id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @HttpCode(204)
+  @Post("/delete/:id")
+  async delete(@Param("id", ParseIntPipe) notification_id: number) {
+    try {
+      await this.notificationService.deleteNotification(notification_id);
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
